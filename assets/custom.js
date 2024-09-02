7e29798dd302a8e15105555d10070a07083828bb
Akbar <jamalakbaralam@gmail.com>
2024-09-02 21:40:17 +0700

    carousel product image in collection using slick

---
+++ assets/custom.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $('.owl-carousel').slick({
+    prevArrow: '<button type="button" class="slick-prev">←</button>',
+    nextArrow: '<button type="button" class="slick-next">→</button>',
+  });
+});
