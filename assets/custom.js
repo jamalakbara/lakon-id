264efb620d4454409246a15fc70a34232233c902
akbar <jamalakbaralam@gmail.com>
2024-09-09 19:24:26 +0700

    update slider homepage, header mobile, footer mobile

--- assets/custom.js
+++ assets/custom.js
@@ -3,4 +3,16 @@ $(document).ready(function(){
     prevArrow: '<button type="button" class="slick-prev">←</button>',
     nextArrow: '<button type="button" class="slick-next">→</button>',
   });
+
+  $('.custom-slideshow--desktop').slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+  $('.custom-slideshow--mobile').slick({
+    prevArrow: '<button type="button" class="slick-prev">←</button>',
+    nextArrow: '<button type="button" class="slick-next">→</button>',
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
 });
