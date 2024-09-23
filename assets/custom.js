5d63a19a898e0b47ec92bba9fe6da976b852b290
akbar <jamalakbaralam@gmail.com>
2024-09-23 14:06:14 +0700

    fix issue swatch on collectio

--- assets/custom.js
+++ assets/custom.js
@@ -38,39 +38,15 @@ $(document).ready(function(){
   // End Slick
 
   $('.card__information fieldset.product-form__input--swatch input').click(function(){
-    console.log($(this))
+    const dataMediaVariant = $(this).attr('data-option-value-id')
+
+    const selectedSlickSlide = $(this).parent().parent().parent().parent().children('.owl-carousel').children('.slick-list').children('.slick-track').children('.slick-slide:not(.slick-cloned)').children().children(`[data-media-variant="${dataMediaVariant}"]`).parent().parent()
+    const dataSlickIndex = $(selectedSlickSlide).attr('data-slick-index')
+
+    const selectedSlickContainer = $(selectedSlickSlide).parent().parent().parent()
+
+    $(selectedSlickContainer).slick('slickGoTo', dataSlickIndex);
   });
-
-  // // get .card__information fieldset.product-form__input--swatch
-  // $('.card__information fieldset.product-form__input--swatch').children().each(function(){
-  //   $(this).click(function(){
-  //     // if $(this) is input element then console.log it
-  //     if ($(this).is('input')) {
-  //       const dataMediaVariant = $(this).attr('data-option-value-id')
-  //       const slickSlides = $(this).parent().parent().parent().parent().children('.owl-carousel').children('.slick-list').children('.slick-track').children('.slick-slide')
-  //       const slickTrack = $(this).parent().parent().parent().parent().children('.owl-carousel').children('.slick-list').children('.slick-track')
-
-  //       $(slickSlides).children().each(function(){
-  //         const figure = $(this).children()
-
-  //         // if figure has data-media-variant then console.log it in jquery
-  //         if ($(figure).attr('data-media-variant') === dataMediaVariant) {
-
-  //           const slickSlide = $(this).parent()
-  //           // if slickSlide doesn't have class name slick-clone then console.log it
-  //           if (!$(slickSlide).hasClass('slick-cloned')) {
-  //             const imageOfSelectedVariant = $(figure).children()
-  //             const currentActiveSlide = $(this).parent().parent().children('.slick-slide.slick-current.slick-active')[0]
-              
-  //             $(currentActiveSlide).children().children().children().replaceWith(imageOfSelectedVariant)
-  //           }
-
-
-  //         }
-  //       });
-  //     }
-  //   });
-  // });
 
   const accordionProductDesc = () => {
     let acc = document.getElementsByClassName("accordion");
